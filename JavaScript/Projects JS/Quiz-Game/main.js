ec22abf8fd94394f4d0d5c39ba50b16566d4110c
ovita-angera <ovita.brian@gmail.com>
2024-04-02 12:04:04 +0300

    python

---
+++ JavaScript/Projects JS/Quiz-Game/main.js
@@ -0,0 +1,6 @@
+// get some console input from our users
+// install a package that can help with the collection of input
+const prompt = require("prompt-sync")()
+
+const value = prompt("Enter your name: ")
+console.log(value);
